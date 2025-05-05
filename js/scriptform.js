f46b6605fdf4cef2588ca6c14c4cb982fc437b3a
Soifiaouir <soifiaouir@gmail.com>
2025-05-05 21:45:39 +0200

    enregistrement multiple dans le storage

--- js/scriptform.js
+++ js/scriptform.js
@@ -12,16 +12,35 @@ form.addEventListener('submit', function(event) {
     const psw = document.getElementById('psw');
     const verifpsw = document.getElementById('mdpverif');
 
-    localStorage.setItem('nom', nom);
-    localStorage.setItem('mail', mail.value);
-    localStorage.setItem('psw', psw.value);
-    localStorage.setItem('verifpsw', verifpsw.value);
+    // localStorage.setItem('nom', nom);
+    // localStorage.setItem('mail', mail.value);
+    // localStorage.setItem('psw', psw.value);
+    // localStorage.setItem('verifpsw', verifpsw.value);
 
     console.log(nom);
 
-    //il me faut maintenant créer une itération de cela pour ne pas écraser les informations de l'utiisateur précedent
+    //création d'un objet
+    let newUtilisateur ={
+        nom : nom,
+        mail :mail,
+        psw : psw,
+    }
+    
+    //recupétation des donnée des utilisateur en les mettants dans un tableau vide
+    let utilisateur = localStorage.getItem("utilisateur");
 
- 
+    if (utilisateur) {
+      utilisateur = JSON.parse(utilisateur);//fonction qui transforme le texte en tableau
+    } else {
+      utilisateur = [];
+    }
+
+    //ajout des nouveaux utilisateurs dans le localStorage
+    utilisateur.push(newUtilisateur);
+
+    const utilisateurText = JSON.stringify(utilisateur);// fonction qui transforme le tableau en texte
+    localStorage.setItem('utilisateur', utilisateurText)
+
 
 
     
