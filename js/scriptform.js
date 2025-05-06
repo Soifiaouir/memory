ebd15ff56302d7ac4e35352256ad94d84d3e97b3
Soifia Ouirdane <soifia.ouirdane2025@campus-eni.fr>
2025-05-06 14:17:58 +0200

    validation nom

--- js/scriptform.js
+++ js/scriptform.js
@@ -8,29 +8,26 @@ form.addEventListener('submit', function(event) {
     //1-Recuperer les donées dans le localStore
     
     const nom = document.getElementById('nom').value;
-    const mail = document.getElementById('mail');
-    const psw = document.getElementById('psw');
-    const verifpsw = document.getElementById('mdpverif');
-
-    // localStorage.setItem('nom', nom);
-    // localStorage.setItem('mail', mail.value);
-    // localStorage.setItem('psw', psw.value);
-    // localStorage.setItem('verifpsw', verifpsw.value);
-
+    const mail = document.getElementById('mail').value;
+    const psw = document.getElementById('psw').value;
+    const verifpsw = document.getElementById('mdpverif').value;
+   
     console.log(nom);
 
     //création d'un objet
-    let newUtilisateur ={
+    // faire attention a ce que le format recuperer ressemble a un format JSON
+    let newUtilisateur = {
         nom : nom,
         mail :mail,
         psw : psw,
+        verifpsw : verifpsw
     }
     
     //recupétation des donnée des utilisateur en les mettants dans un tableau vide
     let utilisateur = localStorage.getItem("utilisateur");
 
-    if (utilisateur) {
-      utilisateur = JSON.parse(utilisateur);//fonction qui transforme le texte en tableau
+   /* if (utilisateur) {
+      utilisateur = JSON.parse(utilisateur);//fonction qui transforme le texte en object
     } else {
       utilisateur = [];
     }
@@ -40,13 +37,20 @@ form.addEventListener('submit', function(event) {
 
     const utilisateurText = JSON.stringify(utilisateur);// fonction qui transforme le tableau en texte
     localStorage.setItem('utilisateur', utilisateurText)
+    */
 
+    //2- Logique de verification des différents champs
 
+    //verif du champs nom  
 
+      if (/^[a-zA-Z]+$/.test(nom) == true){
+        console.log('banane');
+      } else{
+        console.log('panama'); 
+        document.getElementById("messageNom").style.display ="block";
+      }  
     
-
-    
-    
+    //Verif du champ mail
     
     
 
